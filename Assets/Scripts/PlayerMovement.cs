5f432f83be8ef0ec3fce851c65a748a6b7120b51
CunhazadaNoDale <cunha12324@gmail.com>
2026-02-03 21:50:47 -0300

    feat: primeira configuracao para boneco principal andar

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed = 5f;
+    public float horizontalInput, verticalInput;
+    private CharacterController controller;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(horizontalInput, 0, verticalInput);
+        controller.Move(moveDirection * speed * Time.deltaTime);
     }
 }
