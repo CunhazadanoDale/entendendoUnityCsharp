c518ff61fdf2d2d48f7ba3ace0043170ab93c8ea
CunhazadaNoDale <cunha12324@gmail.com>
2026-02-03 22:37:31 -0300

    feat: adição codigo para pular

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,8 +3,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
-    public float horizontalInput, verticalInput;
+    private float horizontalInput, verticalInput;
     private CharacterController controller;
+
+    private Vector3 playerVelocity;
+    private float gravityValue = -9.8f;
+    public float jumpHeight = 1.5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,6 +23,13 @@ public class PlayerMovement : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
 
         Vector3 moveDirection = new Vector3(horizontalInput, 0, verticalInput);
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -2f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(moveDirection * speed * Time.deltaTime);
     }
 }
