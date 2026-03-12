f14098e924d0d1f936ee82d140b359d4dc291f1c
CunhazadaNoDale <cunha12324@gmail.com>
2026-03-11 21:02:39 -0300

    feat: comando para instanciar os sons do jogo

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource[] audioSources;
+    public static AudioManager instance;
+
+    void Awake() {
+        instance = this;
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlaySfx(int indice) {
+        audioSources[indice].Play();
+    }
+}
